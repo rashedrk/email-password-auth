89335875e4d2afc4323aeb6537c051ca79950486
rashedrk <rashedrk001@gmail.com>
2023-04-18 13:40:49 +0600

    integrated with firebase

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,13 +1,21 @@
 import React from 'react';
+import {createUserWithEmailAndPassword, getAuth} from 'firebase/auth'
+import app from '../../firebase/firebase.config';
 
 const Register = () => {
-
+    const auth = getAuth(app);
     const handleSubmit = (event) => {
         event.preventDefault();
         
         const email = event.target.email.value;
         const password = event.target.password.value;
         console.log(email, password);
+        //firebase
+        createUserWithEmailAndPassword(auth, email, password)
+            .then(result => {
+                console.log(result.user);
+            })
+            .catch(error => console.error(error))
     }
 
     return (
